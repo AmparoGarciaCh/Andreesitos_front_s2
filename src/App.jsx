6f4bc332c4823f93df7e8f9352088284ca0ae9b4
“CATALINA <caguirred@uc.cl>
2025-06-04 00:12:16 -0400

    avances pequeños

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Bienvenida from './pages/Bienvenida';
+//import Registro from './pages/Registro';
+//import Login from './pages/Login';
+//import Partidas from './pages/Partidas';
+//import Juego from './pages/Juego';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Bienvenida />} />
+        {/* <Route path="/registro" element={<Registro />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/partidas" element={<Partidas />} /> */}
+        {/* <Route path="/juego/:id" element={<Juego />} /> */}
       </Routes>
     </Router>
   );
--- src/pages/Bienvenida.jsx
+++ src/pages/Bienvenida.jsx
@@ -22,7 +22,7 @@ function Bienvenida() {
       <main className="bienvenida-main">
         <section className="descripcion">
           <h1>WEBTAN</h1>
-          <p><strong>Colonizar, pitutear y construir</strong> son las claves del éxito en la UC.Ubica estratégicamente tus Departamentos, facultades y muros de ingeniería para dominar las mejores casillas, negocia con tus compañeros para conseguir a los especialistas que necesitas y utiliza con astucia al ladrón de cupos para bloquear terrenos rivales. Tal como en la vida universitaria, el pensamiento estratégico y la gestión de recursos son esenciales para destacar frente a los demás jugadores.</p>
+          <p><strong>Colonizar, pitutear y construir</strong> son las claves del éxito en la UC.Ubica estratégicamente tus departamentos, facultades y muros de ingeniería para dominar las mejores casillas, negocia con tus compañeros para conseguir a los especialistas que necesitas y utiliza con astucia al ladrón de cupos para bloquear terrenos rivales. Tal como en la vida universitaria, el pensamiento estratégico y la gestión de recursos son esenciales para destacar frente a los demás jugadores.</p>
           <button className="jugar-btn">Ir a jugar</button>
         </section>
 
